6674d924b6b1b2547f231f57bfb06b09ebbd76d6
Lina Cambridge <lina@yakshaving.ninja>
2019-03-21 23:32:05 -0700

    Fix coverage file globbing on Travis, and link dead code.

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-for file in target/debug/dogear-*[^\.d]; do
+for file in target/debug/dogear-*; do
+  if ! [ -x "${file}" ]; then
+    continue
+  fi
   mkdir -p "target/cov/$(basename $file)"
   kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"
 done
