99c42f54bffe8844e641367d492ea034dd5c76df
lougeniac64 <lougeniaC64@users.noreply.github.com>
2021-04-20 20:15:32 -0400

    Added github actions build config file

--- .travis/before_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-if ! [ -f $HOME/.src/mdbook-$MDBOOK_VERSION ]; then
-  wget -O $HOME/.src/mdbook-$MDBOOK_VERSION.tar.gz https://github.com/rust-lang-nursery/mdBook/releases/download/v$MDBOOK_VERSION/mdbook-v$MDBOOK_VERSION-x86_64-unknown-linux-gnu.tar.gz
-fi
